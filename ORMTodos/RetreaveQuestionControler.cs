2e861617f5e9cb4a18ac219b07326b4265a64e8e
sleepysaurus <ylegaspi@gmail.com>
2014-08-22 16:27:01 +1200

    Last one

---
+++ ORMTodos/RetreaveQuestionControler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ORMTodos
+{
+    class RetreaveQuestionControler: IControler
+    {
+        void IControler.Process(string command, IEnumerable<string> args)
+        {
+           
+
+            using (LightSpeedRepository<Question> retreavingitems = new LightSpeedRepository<Question>())
+            {
+                incomingQuestions = retreavingitems.GetAll.First(o => o.Id == int.Parse(args.First()));
+                Console.WriteLine("Question" + incomingQuestions());
+
+            }
+
+        }
+    }
+}
