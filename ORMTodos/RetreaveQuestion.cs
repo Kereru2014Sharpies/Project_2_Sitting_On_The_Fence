c656cc87e36761e35b1b3d117bb19880dff1ba23
sleepysaurus <ylegaspi@gmail.com>
2014-08-22 14:10:24 +1200

    retreiving done so far

--- ORMTodos/RetreaveQuestion.cs
+++ ORMTodos/RetreaveQuestion.cs
@@ -5,7 +5,19 @@ using System.Text;
 
 namespace ORMTodos
 {
-    class RetreaveQuestion
+    class RetreaveQuestion: IControler
     {
+        void IControler.Process(string command, IEumerable<strings> args)
+        {
+            DBScript dbscript;
+
+            using (LightSpeedRepository<DBScript> retreavingitems = new LightSpeedRepository<DBScript>())
+            {
+                incomingQuestions = dbscript.retreavingitems.First(o => o.Id == int.Parse(args.First()));
+                Console.WriteLine("Question" + incomingQuestions());
+
+            }
+
+        }
     }
 }
